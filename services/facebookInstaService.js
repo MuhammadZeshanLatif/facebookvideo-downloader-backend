ab049c7d50e06c3652c1050d232ec3bef497c24e
muzeeshan-dd <muzeeshan@digital-dividend.com>
2026-02-16 19:28:23 +0500

    update

--- services/facebookInstaService.js
+++ services/facebookInstaService.js
@@ -1,6 +1,8 @@
+require("cheerio");
+const snapsave = require("metadownloader");
+
 async function facebookInsta(url) {
   try {
-    const snapsave = require("metadownloader");
     const result = await snapsave(url); // or snapsave.facebook(url) for FB links
     return result;
   } catch (error) {
